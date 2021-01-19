8b5ad88d28d50dd4e75e9fcf4b68b74bb0adb5e8
Michael Ramuta <michaelramuta@gmail.com>
2021-01-19 15:21:53 -0800

    create mapping paths table

---
+++ src/migrations/Install.php
@@ -0,0 +1,25 @@
+<?php
+namespace onedesign\onesolr\migrations;
+
+use craft\db\Migration;
+
+class Install extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%onesolr_mappingpaths}}', [
+            'id' => $this->primaryKey(),
+            'sectionId' => $this->integer()->notNull(),
+            'mappingPath' => $this->string()->notNull(),
+            'locale' => $this->string()->notNull(),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid(),
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%onesolr_mappingpaths}}');
+    }
+}
