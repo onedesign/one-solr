a681b705c92a46df32f22bd0fc5b5bd8f8942ebd
Michael Ramuta <michaelramuta@gmail.com>
2021-01-19 12:37:12 -0800

    remove function tests

--- tests/functional/ExampleFunctionalCest.php
+++ tests/functional/ExampleFunctionalCest.php
@@ -7,18 +7,4 @@ use FunctionalTester;
 
 class ExampleFunctionalCest
 {
-    // Public methods
-    // =========================================================================
-
-    // Tests
-    // =========================================================================
-
-    /**
-     *
-     */
-    public function testCraftEdition(FunctionalTester $I)
-    {
-        $I->amOnPage('?p=/');
-        $I->seeResponseCodeIs(200);
-    }
 }
